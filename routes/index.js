1399f37b2c1dbe3e4bb2722d6e212eff0f3ebb18
gandhyman <gandhycoder@gmail.com>
2022-06-19 03:06:33 -0600

    Add: Rae definitions

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,51 @@
 const { Router } = require('express')
 const router = Router()
+const { RAE } = require('rae-api');
+const rae = new RAE(true);
 
 const words = require('../dict')
 const json = []
-words.map((e)=> {
-        json.push({
-            word: e,
-            length: e.length
-        })
+const raeDefs = async (e) => {
+    try {
+        const search = await rae.searchWord(e);
+        const wordId = search.getRes()[0].getId(); // gets e word id
+        const result = await rae.fetchWord(wordId); // fetches the word as object
+        const definitions = result.getDefinitions(); // gets all 'hola' definitions as Defintion[]
+        const first = definitions[0].getDefinition()
+        let res = []
+        let i = 0
+        for (const definition of definitions) {
+            res.push({
+                id: i,
+                Tipo: definition.getType(),
+                Definición: definition.getDefinition()
+            })
+            i++
+        }
+
+        return res
+    } catch (e){
+        console.log("Error conecting RAE: ", e)
+    }
+}
+
+words.map( e => { 
+    json.push({
+        word: e,
+        length: e.length,
     })
+})
 
 // Raiz
 router.get('/', (req, res) => {
     res.json(json)
+})
+
+router.get('/def/:word', async (req, res) => {
+    const word = req.params.word
+    res.json({
+        [word]: await raeDefs(word)
+    })
 })
 
 module.exports = router
